27ce4628b21ae2e2551375b724639abf2daa40bb
tonyishangu <tonyishangu@gmail.com>
2020-11-02 10:26:39 +0300

    add all hoods file

--- hood/urls.py
+++ hood/urls.py
@@ -1,10 +1,23 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.conf.urls import url,include
 from . import views
 
 
 urlpatterns = [
+    url('^', views.index, name='index'),
+    url('^register/', views.signup, name='signup'),
+    url('^account/', include('django.contrib.auth.urls')),
+    url('^all-hoods/', views.hoods, name='hood'),
+    url('^new-hood/', views.create_hood, name='new-hood'),
+    url('^profile/<username>', views.profile, name='profile'),
+    url('^profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+    url('^join_hood/<id>', views.join_hood, name='join-hood'),
+    url('^leave_hood/<id>', views.leave_hood, name='leave-hood'),
+    url('^single_hood/<hood_id>', views.single_hood, name='single-hood'),
+    url('^<hood_id>/new-post', views.create_post, name='post'),
+    url('^<hood_id>/members', views.hood_members, name='members'),
+    url('^search/', views.search_business, name='search'),
     url('^api/profiles/$', views.ProfileList.as_view(), name='profile_list'),
     url('^api/neighbourhood/$', views.NeighbourhoodList.as_view(),
         name='neighbourhood_list'),
--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -123,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
