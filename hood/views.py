fe3267af33f8e1b5deb0b20b0d82bcc2645ca7fc
tonyishangu <tonyishangu@gmail.com>
2020-11-02 09:37:07 +0300

    signup views

--- hood/views.py
+++ hood/views.py
@@ -13,3 +13,17 @@ from .serializer import ProfileSerializer,NeighbourhoodSerializer,Business_centr
 @login_required
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
