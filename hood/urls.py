ffd449c108a08c282de1d667b91a9c5719db690d
tonyishangu <tonyishangu@gmail.com>
2020-11-02 10:43:09 +0300

    content

--- hood/urls.py
+++ hood/urls.py
@@ -1,23 +1,24 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url,include
+from django.conf.urls import url
 from . import views
+from django.urls import path, include
 
 
 urlpatterns = [
-    url('^', views.index, name='index'),
-    url('^register/', views.signup, name='signup'),
-    url('^account/', include('django.contrib.auth.urls')),
-    url('^all-hoods/', views.hoods, name='hood'),
-    url('^new-hood/', views.create_hood, name='new-hood'),
-    url('^profile/<username>', views.profile, name='profile'),
-    url('^profile/<username>/edit/', views.edit_profile, name='edit-profile'),
-    url('^join_hood/<id>', views.join_hood, name='join-hood'),
-    url('^leave_hood/<id>', views.leave_hood, name='leave-hood'),
-    url('^single_hood/<hood_id>', views.single_hood, name='single-hood'),
-    url('^<hood_id>/new-post', views.create_post, name='post'),
-    url('^<hood_id>/members', views.hood_members, name='members'),
-    url('^search/', views.search_business, name='search'),
+    path('', views.index, name='index'),
+    path('register/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('all-hoods/', views.hoods, name='hood'),
+    path('new-hood/', views.create_hood, name='new-hood'),
+    path('profile/<username>', views.profile, name='profile'),
+    path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+    path('join_hood/<id>', views.join_hood, name='join-hood'),
+    path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
+    path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
+    path('<hood_id>/new-post', views.create_post, name='post'),
+    path('<hood_id>/members', views.hood_members, name='members'),
+    path('search/', views.search_business, name='search'),
     url('^api/profiles/$', views.ProfileList.as_view(), name='profile_list'),
     url('^api/neighbourhood/$', views.NeighbourhoodList.as_view(),
         name='neighbourhood_list'),
