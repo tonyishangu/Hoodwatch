9dd36ea30d513ffec256a37ca59f64d3d669d8ce
tonyishangu <tonyishangu@gmail.com>
2020-11-02 09:32:12 +0300

    add b/s models

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Business_centres(models.Model):
+    centre_name = models.CharField(max_length=20)
+    contact_info = models.CharField(max_length=20)
+    emergency_service = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.centre_name
+
+    def save_centre(self):
+        self.save()
+
+    def delete_centre(self):
+        self.delete()
